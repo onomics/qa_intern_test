f44de6f002f4ec2faab2d975cc351ad335adb731
onomics <141557463+onomics@users.noreply.github.com>
2023-08-27 21:59:57 +0500

    commit autotest

---
+++ task/favorites_test.js
@@ -0,0 +1,10 @@
+Feature('Избранное');
+
+Scenario('Добавление объявления в избранное из карточки товара', ({I}) => {
+    I.amOnPage('/nikel/knigi_i_zhurnaly/domain-driven_design_distilled_vaughn_vernon_2639542363');
+    I.waitForElement('//div[contains(@class, "style-header-add-favorite")]//button[@data-marker="item-view/favorite-button"]', 20);
+    I.click('//div[contains(@class, "style-header-add-favorite")]//button[@data-marker="item-view/favorite-button"]');
+    I.click('//a[contains(@class, "index-counter")]');
+    I.waitForElement('//*[.="Domain-Driven Design Distilled Vaughn Vernon"]', 20);
+    I.seeElement('//*[.="Domain-Driven Design Distilled Vaughn Vernon"]');
+})
