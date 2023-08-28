35c5d54038ccd4be67e5e53246325d002b7c7087
onomics <141557463+onomics@users.noreply.github.com>
2023-08-28 15:11:46 +0500

    creating constants

--- task/favorites_test.js
+++ task/favorites_test.js
@@ -1,11 +1,16 @@
 Feature('Избранное');
 
+const addToFavorite = '//div[contains(@class, "style-header-add-favorite")]//button[@data-marker="item-view/favorite-button"]';
+const inFavoriteButton = '//div[contains(@class, "style-header-add-favorite")]//button[@data-is-favorite="true"]';
+const linkToFavorite = '//a[contains(@class, "index-counter") and @data-marker="header/favorites"]';
+const itemTitle = '//*[.="Domain-Driven Design Distilled Vaughn Vernon"]';
+
 Scenario('Добавление объявления в избранное из карточки товара', ({I}) => {
     I.amOnPage('/nikel/knigi_i_zhurnaly/domain-driven_design_distilled_vaughn_vernon_2639542363');
-    I.waitForElement('//div[contains(@class, "style-header-add-favorite")]//button[@data-marker="item-view/favorite-button"]', 20);
-    I.click('//div[contains(@class, "style-header-add-favorite")]//button[@data-marker="item-view/favorite-button"]');
-    I.waitForElement('//div[contains(@class, "style-header-add-favorite")]//button[@data-is-favorite="true"]', 20)
-    I.click('//a[contains(@class, "index-counter")]');
-    I.waitForElement('//*[.="Domain-Driven Design Distilled Vaughn Vernon"]', 20);
-    I.seeElement('//*[.="Domain-Driven Design Distilled Vaughn Vernon"]');
+    I.waitForElement(addToFavorite, 20);
+    I.click(addToFavorite);
+    I.waitForElement(inFavoriteButton, 20);
+    I.click(linkToFavorite);
+    I.waitForElement(itemTitle, 20);
+    I.seeElement(itemTitle);
 })
